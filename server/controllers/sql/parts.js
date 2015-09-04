f03ce835b927214ce712266c54e9de30ff329683
Jason Pero <jpero09@gmail.com>
2015-09-04 13:06:51 +0000

    Adding missing jscs cleanup

--- server/controllers/sql/parts.js
+++ server/controllers/sql/parts.js
@@ -3,8 +3,9 @@ var ctrlBase = require('../base/parts');
 var pg = require('pg');
 
 var SELECT_ALL = 'select * from swfc.parts ORDER BY name;';
-var SELECT_BY_ID = 'SELECT * FROM swfc.parts WHERE id = $1;'
-var SELECT_VEHICLES = 'SELECT * FROM swfc.vehicles v LEFT JOIN swfc."vehicleParts" vp on vp.partid = v.id WHERE vp.partid = $1;'
+var SELECT_BY_ID = 'SELECT * FROM swfc.parts WHERE id = $1;';
+var SELECT_VEHICLES = 'SELECT * FROM swfc.vehicles v LEFT JOIN swfc."vehicleParts"' +
+  ' vp on vp.partid = v.id WHERE vp.partid = $1;';
 
 var Parts = function(options) {
   Parts.super_.call(this, options); // Call the base init
@@ -37,7 +38,7 @@ Parts.prototype.Get = function(callback) {
 
 Parts.prototype.GetByID = function(id, callback) {
   var self = this;
-  var output; 
+  var output;
 
   pg.connect(self.databaseUrl, function(err, client, done) {
     if(err) {
@@ -65,7 +66,7 @@ Parts.prototype.GetByID = function(id, callback) {
 
 Parts.prototype.GetVehicles = function(id, callback) {
   var self = this;
-  var output = []; 
+  var output = [];
 
   pg.connect(self.databaseUrl, function(err, client, done) {
     if(err) {
